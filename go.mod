d86e80572ee8cd491c2985f880fa57cbe4a410e3
unknown <anirban.techlead@gmail.com>
2024-12-19 12:23:09 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anirban-techbatch/poc-sum
+
+go 1.23.4
---
+++ sum.go
@@ -0,0 +1,7 @@
+// sum.go
+package sum
+
+// Add takes two integers and returns their sum
+func Add(a, b int) int {
+	return a + b
+}
